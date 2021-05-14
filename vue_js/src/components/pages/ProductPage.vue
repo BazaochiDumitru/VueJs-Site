1f1586983c7d9c1e88fe09e1fa6c428bf7a56e24
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-05-14 17:28:24 +0300

    1.6

--- vue_js/src/components/pages/ProductPage.vue
+++ vue_js/src/components/pages/ProductPage.vue
@@ -1,14 +1,20 @@
 <template>
+<section>
     <Header/>
+    <Product/>
+    <Footer/>
+</section>
 </template>
 
 <script>
     import Header from '@/components/views/header/Header'
+    import Product from '@/components/views/product-page/product'
+    import Footer from "@/components/views/footer/Footer";
 
     export default {
         name: "ProductPage",
         components : {
-            Header
+            Header, Product, Footer
         }
     }
 </script>
---
+++ vue_js/src/components/views/product-page/product.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h2>This is product page</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Product",
+};
+</script>
+
+<style scoped>
+h2 {
+    margin-top: 100px;
+    text-align: center;
+}
+</style>
