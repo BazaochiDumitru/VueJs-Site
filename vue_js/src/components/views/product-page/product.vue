f8f9de5cc21a442d7a4322a5f9966febfd119550
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-05-14 18:30:15 +0300

    1.6

--- vue_js/src/components/views/product-page/product.vue
+++ vue_js/src/components/views/product-page/product.vue
@@ -1,7 +1,117 @@
 <template>
-  <section>
-    <h2>This is product page</h2>
-  </section>
+  <div>
+    <h2>Products</h2>
+    <section class="categories">
+      <div class="container__product">
+        <div class="card" id="cardItems">
+          <article class="prod">
+            <div class="item-container" data-id="1">
+              <div class="card-item">
+                <img
+                  :src="require('../../../../static/img/macbook.png')"
+                  alt=""
+                  class="card-img"
+                />
+                <p class="item-name">Apple Macbook Pro</p>
+                <p class="item-description">
+                  Apple Macbook Pro 15 G5 Black (5500)+W10H, 15.6'' 300Hz WVA
+                  FHD300 nits (IntelCore i7-10750H, 2.6-5.0GHz, 16GB (2x8) DDR4
+                  RAM, 1TB M.2 PCIe SSD, GeForce GTX 1660Ti 6GB GDDR6CR,
+                  WiFi6-AC/BT5, 4cell, HD Webcam, Backlit KB,RUS,Win10H,2.34kg).
+                </p>
+              </div>
+              <h3 class="item-price">
+                Price : <span class="item-price-value">1200</span> $
+              </h3>
+              <em
+                ><button class="product__btn btn add-cart">
+                  Add to cart
+                </button></em
+              >
+            </div>
+          </article>
+          <article class="prod">
+            <div class="item-container" data-id="1">
+              <div class="card-item">
+                <img
+                  :src="require('../../../../static/img/macbook.png')"
+                  alt=""
+                  class="card-img"
+                />
+                <p class="item-name">Apple Macbook Pro</p>
+                <p class="item-description">
+                  Apple Macbook Pro 15 G5 Black (5500)+W10H, 15.6'' 300Hz WVA
+                  FHD300 nits (IntelCore i7-10750H, 2.6-5.0GHz, 16GB (2x8) DDR4
+                  RAM, 1TB M.2 PCIe SSD, GeForce GTX 1660Ti 6GB GDDR6CR,
+                  WiFi6-AC/BT5, 4cell, HD Webcam, Backlit KB,RUS,Win10H,2.34kg).
+                </p>
+              </div>
+              <h3 class="item-price">
+                Price : <span class="item-price-value">1200</span> $
+              </h3>
+              <em
+                ><button class="product__btn btn add-cart">
+                  Add to cart
+                </button></em
+              >
+            </div>
+          </article>
+          <article class="prod">
+            <div class="item-container" data-id="1">
+              <div class="card-item">
+                <img
+                  :src="require('../../../../static/img/macbook.png')"
+                  alt=""
+                  class="card-img"
+                />
+                <p class="item-name">Apple Macbook Pro</p>
+                <p class="item-description">
+                  Apple Macbook Pro 15 G5 Black (5500)+W10H, 15.6'' 300Hz WVA
+                  FHD300 nits (IntelCore i7-10750H, 2.6-5.0GHz, 16GB (2x8) DDR4
+                  RAM, 1TB M.2 PCIe SSD, GeForce GTX 1660Ti 6GB GDDR6CR,
+                  WiFi6-AC/BT5, 4cell, HD Webcam, Backlit KB,RUS,Win10H,2.34kg).
+                </p>
+              </div>
+              <h3 class="item-price">
+                Price : <span class="item-price-value">1200</span> $
+              </h3>
+              <em
+                ><button class="product__btn btn add-cart">
+                  Add to cart
+                </button></em
+              >
+            </div>
+          </article>
+          <article class="prod">
+            <div class="item-container" data-id="1">
+              <div class="card-item">
+                <img
+                  :src="require('../../../../static/img/macbook.png')"
+                  alt=""
+                  class="card-img"
+                />
+                <p class="item-name">Apple Macbook Pro</p>
+                <p class="item-description">
+                  Apple Macbook Pro 15 G5 Black (5500)+W10H, 15.6'' 300Hz WVA
+                  FHD300 nits (IntelCore i7-10750H, 2.6-5.0GHz, 16GB (2x8) DDR4
+                  RAM, 1TB M.2 PCIe SSD, GeForce GTX 1660Ti 6GB GDDR6CR,
+                  WiFi6-AC/BT5, 4cell, HD Webcam, Backlit KB,RUS,Win10H,2.34kg).
+                </p>
+              </div>
+              <h3 class="item-price">
+                Price : <span class="item-price-value">1200</span> $
+              </h3>
+              <em
+                ><button class="product__btn btn add-cart">
+                  Add to cart
+                </button></em
+              >
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -12,7 +122,194 @@ export default {
 
 <style scoped>
 h2 {
-    margin-top: 100px;
+  margin-top: 100px;
+  margin-left: 100px;
+  margin-bottom: 50px;
+}
+.container__product {
+  width: 1244px;
+  padding: 0 20px;
+  margin: 100px auto;
+}
+
+.prod {
+  /* width: 285px; */
+  border: 1px solid #d9d9d9;
+  border-radius: 4px;
+  padding: 20px;
+  padding-top: 9px;
+  font-size: 14px;
+  background-color: #fff;
+}
+
+.categories {
+  margin-top: 25px;
+}
+
+.containers {
+  margin: 17%;
+  margin-top: 10px;
+}
+
+.card {
+  text-align: center;
+  font-family: sans-serif;
+  grid-column: 3 span;
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(4, 1fr);
+}
+
+.item-container {
+  background: white;
+  position: relative;
+  background: white;
+  padding: 15px 12px;
+  position: relative;
+}
+
+.card-item {
+  cursor: pointer;
+}
+
+.card-img {
+  position: relative;
+  max-width: 100%;
+  max-height: 8rem;
+  text-align: center;
+}
+
+.item-name {
+  text-align: center;
+  color: rgb(0, 0, 0);
+  margin-top: 1rem;
+  font-size: 0.95rem;
+}
+.item-description {
+  text-align: justify;
+  color: rgb(0, 0, 0);
+  margin-top: 1rem;
+  font-size: 0.95rem;
+}
+
+.add-to-cart {
+  font-size: 1.5rem;
+  color: rgb(166, 0, 0);
+  position: absolute;
+  bottom: 1rem;
+  right: 0.8rem;
+  cursor: pointer;
+  opacity: 0;
+}
+
+.item-price {
+  color: black;
+  text-align: center;
+  margin-top: 0.5rem;
+}
+
+.product__btn {
+  width: 100%;
+}
+
+.product__btn:disabled {
+  opacity: 0.4;
+  pointer-events: none;
+}
+
+.btn {
+  border: none;
+  padding: 14px 0;
+  border-radius: 4px;
+  color: #fff;
+  background-color: #5d71dd;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.product__btn:hover {
+  background-color: #6078f1;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.btn:hover {
+  background-color: #6078f1;
+  transition: background-color 0.3s ease-in-out;
+}
+
+@media only screen and (max-width: 1200px) {
+  .home {
+    margin-left: 50px;
+  }
+  .card {
     text-align: center;
+    font-family: sans-serif;
+    grid-column: 3 span;
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .container__product {
+    width: 100%;
+    padding: 0 20px;
+    margin: 50px auto;
+  }
+}
+
+@media only screen and (max-width: 998px) {
+  .home {
+    /* margin-top: -20px; */
+    margin-left: -10px;
+  }
+  .card {
+    text-align: center;
+    font-family: sans-serif;
+    grid-column: 3 span;
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .container__product {
+    width: 100%;
+    padding: 0 20px;
+    margin: 50px auto;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .card {
+    text-align: center;
+    font-family: sans-serif;
+    grid-column: 3 span;
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+  .container__product {
+    width: 100%;
+    padding: 0 20px;
+    margin: 50px auto;
+  }
+}
+
+@media only screen and (max-width: 568px) {
+    h2 {
+  margin-top: 100px;
+  margin-left: 50px;
+  margin-bottom: 50px;
+}
+  .card {
+    text-align: center;
+    font-family: sans-serif;
+    grid-column: 3 span;
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: 1fr;
+  }
+  .container__product {
+    width: 100%;
+    padding: 0 20px;
+    margin: 50px auto;
+  }
 }
 </style>
